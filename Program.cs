af3c21072f0e93e05fc82cb290dd2fdc1895ba27
name <varunbrahmane30@gmail.com>
2021-07-29 18:53:06 +0530

    [varun] added: User regisration prob using ANNOTATION

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day22_UserRegistrationUsingAnnotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" User Registration Problem Using Annotations ");
+        }
+    }
+}
