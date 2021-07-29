fb45dabc5a25721af1d2a33c4eee085b8b4e13ae
name <varunbrahmane30@gmail.com>
2021-07-29 19:24:52 +0530

    [varun] added: User data display calling

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Day22_UserRegistrationUsingAnnotation
         static void Main(string[] args)
         {
             Console.WriteLine(" User Registration Problem Using Annotations ");
+            AnnotationForUser.Display();
         }
     }
 }
